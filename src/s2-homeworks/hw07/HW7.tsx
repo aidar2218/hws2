146b60741cda2b85f2b83911b1cb52eb1a241a36
Aidar Ibraev <aidaribraev32@gmail.com>
2024-10-30 06:07:54 +0600

    Commit 3.3.1. HW7.

--- src/s2-homeworks/hw07/HW7.tsx
+++ src/s2-homeworks/hw07/HW7.tsx
@@ -12,12 +12,12 @@ import s from './HW7.module.css'
 * */
 
 const arr = [
-    { id: 1, value: 'x' },
-    { id: 2, value: 'y' },
-    { id: 3, value: 'z' },
+    { id: 1, value: 'Pre-Junior' },
+    { id: 2, value: 'Junior' },
+    { id: 3, value: 'Junior +' },
 ] // value может быть изменено
 
-const HW7 = () => {
+export const HW7: React.FC = () => {
     const [value, onChangeOption] = useState(1) // селект и радио должны работать синхронно
 
     return (
@@ -25,7 +25,7 @@ const HW7 = () => {
             <div className={s2.hwTitle}>Homework #7</div>
 
             {/*демонстрация возможностей компонент:*/}
-            <div className={s2.hw}>
+            <div className={`${s2.hw} ${s2.hw_7}`}>
                 <div className={s.container}>
                     <div>
                         <SuperSelect
@@ -49,5 +49,3 @@ const HW7 = () => {
         </div>
     )
 }
-
-export default HW7
